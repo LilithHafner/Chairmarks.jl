a16ddfb13e6f4ba51d6794e1b794823b64cac371
Documenter.jl <documenter@juliadocs.github.io>
2023-11-11 01:35:21 +0000

    build based on f5b9c2f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Tablemarks","category":"page"},{"location":"#Tablemarks","page":"Home","title":"Tablemarks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Tablemarks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Tablemarks]","category":"page"}]
+}
