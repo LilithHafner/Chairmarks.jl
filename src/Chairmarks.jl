9c3a7c6b0581f18a40a2af41f1abd18ba2485d05
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-02 21:05:21 -0600

    remove links in module docstring

--- src/Chairmarks.jl
+++ src/Chairmarks.jl
@@ -7,9 +7,9 @@ julia> @b evalpooly(rand(), rand(100))
 198.077 ns (2 allocs: 928 bytes)
 ```
 
-Documentation: [https://chairmarks.lilithhafner.com/stable/](@ref index)
+Documentation: https://chairmarks.lilithhafner.com/stable
 
-Tutorial: [https://chairmarks.lilithhafner.com/stable/tutorial.html](@ref tutorial)
+Tutorial: https://chairmarks.lilithhafner.com/stable/tutorial.html
 
 Reference: see docstrings of [`@be`](@ref) and [`@b`](@ref)
 """
