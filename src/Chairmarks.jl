f3059c0bc36b21e4ca09c87160a1946e29ab9d5c
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-03 14:31:24 +0000

    edit docstring that has existing typo

--- src/Chairmarks.jl
+++ src/Chairmarks.jl
@@ -2,6 +2,8 @@
 Chairmarks provides the macros [`@b`](@ref) and [`@be`](@ref) for benchmarking code. See
 their respecitve docstrings for usage.
 
+Editing this docstring
+
 ```julia
 julia> @b evalpooly(rand(), rand(100))
 198.077 ns (2 allocs: 928 bytes)
