cf01dc93c23da9b52da22d482375283217db2130
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-04 13:36:56 -0600

    integrate into macro_tools and document substitute_underscores

--- src/macro_tools.jl
+++ src/macro_tools.jl
@@ -1,6 +1,12 @@
-substitute(f::Symbol, var::Symbol) = f === :_ ? (var, true) : (f, false)
-substitute(f, ::Symbol) = f, false
-function substitute(ex::Expr, var::Symbol)
+"""
+    substitute_undescores(expr, var) -> new, changed
+
+Replace all occurances of `_` in `expr` with `var` and return the new expression and a Bool
+indicating whether the expression was changed.
+"""
+substitute_undescores(f::Symbol, var::Symbol) = f === :_ ? (var, true) : (f, false)
+substitute_undescores(f, ::Symbol) = f, false
+function substitute_undescores(ex::Expr, var::Symbol)
     changed = false
     args = similar(ex.args)
     i = firstindex(args)
@@ -9,10 +15,31 @@ function substitute(ex::Expr, var::Symbol)
         i += 1
     end
     for i in i:lastindex(args)
-        args[i], c = substitute(ex.args[i], var)
+        args[i], c = substitute_undescores(ex.args[i], var)
         changed |= c
     end
     changed ? exprarray(ex.head, args) : ex, changed
+end
+
+"""
+    extract_interpolations!(interpolations, expr) -> expr
+
+Replaces any \$ interpolations in `expr` with gensyms and adds the gensym/expression pairs
+to `interpolations`. Return the modified `expr`.
+
+May mutate the `args` of input `Expr`s.
+"""
+extract_interpolations!(interpolations, expr) = expr
+function extract_interpolations!(interpolations, expr::Expr)
+    if expr.head === :$
+        sym = gensym("interpolation")
+        push!(interpolations, Expr(:(=), sym, only(expr.args)))
+        sym
+    else
+        exprarray(expr.head, map!(expr.args, expr.args) do arg
+            extract_interpolations!(interpolations, arg)
+        end)
+    end
 end
 
 create_first_function(f::Symbol) = f
@@ -21,7 +48,7 @@ create_first_function(body::Expr) = :(() -> $body)
 function create_function(f)
     f === :_ && return identity
     var = gensym()
-    new, changed = substitute(f, var)
+    new, changed = substitute_undescores(f, var)
     changed ? :($var -> $new) : f
 end
 function process_args(exprs)
@@ -30,6 +57,7 @@ function process_args(exprs)
     in_kw = false
     parameters = Any[]
     args = Any[benchmark, exprarray(:parameters, parameters)]
+    interpolations = Any[]
     for ex in exprs
         if ex isa Expr && ex.head === :(=) && ex.args[1] isa Symbol
             in_kw = true
@@ -39,12 +67,16 @@ function process_args(exprs)
             error("Positional argument after keyword argument")
         elseif ex === :_
             push!(args, nothing)
-        elseif first
-            push!(args, create_first_function(ex))
-            first = false
         else
-            push!(args, create_function(ex))
+            ex2 = extract_interpolations!(interpolations, ex)
+            if first
+                push!(args, create_first_function(ex2))
+                first = false
+            else
+                push!(args, create_function(ex2))
+            end
         end
     end
-    esc(exprarray(:call, args))
+    call = exprarray(:call, args)
+    esc(isempty(interpolations) ? call : Expr(:let, exprarray(:block, interpolations), Expr(:block, call)))
 end
