0957751b2eb5a138eba5c3e6350e83d4dd28fd0b
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-08 16:41:47 +0000

    micro-simplify tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -318,7 +318,7 @@ using Chairmarks: Sample, Benchmark
                 fres.time > gres.time
             end
 
-            VERSION > v"1.8" && @test sum([check() for _ in 1:10]) >= 8 # Needs @noinline at callsite
+            VERSION > v"1.8" && @test sum(check() for _ in 1:10) >= 8 # Needs @noinline at callsite
         end
     end
 
