f42c7f47a2d45629da268354e3490cdb571baa3b
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-08 10:33:55 -0600

    fix syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -318,7 +318,7 @@ using Chairmarks: Sample, Benchmark
                 fres.time > gres.time
             end
 
-            VERSION >= v"1.8" && @test sum(check() for _ in 1:10) >= 8 # Needs Base.donotdelete
+            VERSION >= v"1.8" && (@test sum(check() for _ in 1:10) >= 8) # Needs Base.donotdelete
         end
     end
 
