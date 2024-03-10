58da145b7249efae407d7dd9031bd2cdc770611f
Documenter.jl <documenter@juliadocs.github.io>
2024-03-10 16:40:14 +0000

    build based on 1cdfb9d

---
+++ previews/PR80/assets/why.md.DjnXz4zM.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,a7 as t}from"./chunks/framework.CaOF6C0E.js";const o=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"why.md","filePath":"why.md","lastUpdated":null}'),n={name:"why.md"},h=t("",23),l=[h];function e(k,p,r,d,E,g){return a(),i("div",null,l)}const c=s(n,[["render",e]]);export{o as __pageData,c as default};
