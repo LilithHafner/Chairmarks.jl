598d59956cd593f8cb22c1109ed72ea93f279548
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-25 11:15:56 -0400

    Add a method to `Statistics.std` for Benchmarks
    Pretty simple change, just what it says on the tin.

--- ext/StatisticsChairmarksExt.jl
+++ ext/StatisticsChairmarksExt.jl
@@ -5,5 +5,6 @@ using Statistics, Chairmarks
 Statistics.mean(x::Chairmarks.Benchmark) = Chairmarks.elementwise(mean, x)
 Statistics.median(x::Chairmarks.Benchmark) = Chairmarks.elementwise(median, x)
 Statistics.quantile(x::Chairmarks.Benchmark, p; kws...) = Chairmarks.elementwise(y -> quantile(y, p; kws...), x)
+Statistics.std(x::Chairmarks.Benchmark; kws...) = Chairmarks.elementwise(y -> std(y; kws...), x)
 
 end
