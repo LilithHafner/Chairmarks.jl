55417d7d75d4d20c1e15dd67ee369bee5c25f1c8
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-09-14 09:15:35 -0500

    fix typo

--- src/public.jl
+++ src/public.jl
@@ -217,7 +217,7 @@ A global constant that holds default benchmarking parameters.
 When a parameter is unspecified it defaults to the value stored in `Chairmarks.DEFAULTS`.
 
 Currently there is one stable default: `Chairmarks.DEFAULTS.seconds::Float64` which defaults
-to 0.1; and one expiremental default: `Chairmarks.DEFAULTS.gc::Bool` which defaults to
+to 0.1; and one experimental default: `Chairmarks.DEFAULTS.gc::Bool` which defaults to
 `true`.
 
 All default values may be changed in the future and the `gc` default may be removed
