ea4d2bdf3ef7b8f68b51167f4c551640c31c10fe
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-11-23 18:51:49 -0600

    cleanup precompile NFC
    At this point, TTFX and load time are better than the most recently released version (1.2.2)
    
    ```julia-repl
    julia> f(expr; old=false, show=false) = eval(Meta.parse(read(`julia $(old ? () : "--project") $(show ? "--trace-compile=stderr" : ()) -E $("a = @elapsed using Chairmarks; a,@elapsed @eval $expr")`, String)))
    f (generic function with 3 methods)
    
    julia> comp(x) = f(x, old=true) => f(x)
    comp (generic function with 1 method)
    
    julia> comp("@b rand evals=1 samples=1")
    (0.008823733, 0.038952421) => (0.007316222, 0.033092877)
    
    julia> comp("@b rand hash seconds=.001")
    (0.00852394, 0.046460937) => (0.007173346, 0.037048532)
    
    julia> comp("@b rand hash(_) seconds=.001")
    (0.00833723, 0.050975096) => (0.007070262, 0.03884667)
    
    julia> comp("@b rand hash evals=1 samples=1")
    (0.008731608, 0.042089029) => (0.00729393, 0.03947555)
    
    julia> comp("@b rand hash(_),hash evals=1 samples=1") # This has a major behavior change
    (0.009230821, 0.048152574) => (0.007140638, 0.324881723)
    ```

--- src/precompile.jl
+++ src/precompile.jl
@@ -3,10 +3,5 @@ precompile(Tuple{typeof(Chairmarks.create_first_function), Symbol})
 precompile(Tuple{typeof(Chairmarks.create_function), Symbol})
 precompile(Tuple{typeof(Chairmarks.create_first_function), Expr})
 precompile(Tuple{typeof(Chairmarks.create_function), Expr})
-# precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:seconds,), Tuple{Float64}}, typeof(Chairmarks.benchmark), Function, Function})
 precompile(Tuple{typeof(Chairmarks.benchmark), Any, Any, Any, Int64, Int64, Float64, Bool, Any})
 precompile(Tuple{typeof(Chairmarks.summarize), Chairmarks.Benchmark})
-
-# Comparison
-# precompile(Tuple{typeof(Base.map!), typeof(Chairmarks.create_function), Array{Any, 1}, Array{Any, 1}})
-
